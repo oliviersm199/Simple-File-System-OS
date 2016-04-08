f109b377d25e7d6925f970ee5a8933d810573bc3
olivier.simard-morissette@mail.mcgill.ca <olivier.morissette@gmail.com>
2016-04-07 22:28:34 -0400

    Initial testing, segmentation fault due to strncpy in sfs_fwrite

--- jit_test.c
+++ jit_test.c
@@ -10,5 +10,12 @@
 
 int main() {
     mksfs(1);    
+    int ptr = sfs_fopen("new_file.txt");
+    printf("File Open:%d\n",ptr);
+    char * hello = "HELLO WORLD I AM FINE AND YOU?";
+    int write = sfs_fwrite(ptr,hello,sizeof(hello));
+    printf("Write:%d\n",write);
+    char  buffer[1024];
+    int read = sfs_fread(ptr,buffer,sizeof(hello));
     return 0; 
 }
